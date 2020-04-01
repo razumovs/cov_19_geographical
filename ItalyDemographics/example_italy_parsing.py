d53ca0ae1d05970c6e32bb556a485c350211d0c7
razumovs <razumovs@gmail.com>
2020-04-01 09:21:53 -0700

    added get_number_smoker_share_by_region

--- ItalyDemographics/example_italy_parsing.py
+++ ItalyDemographics/example_italy_parsing.py
@@ -5,9 +5,11 @@ region_density_information = get_list_population_stat_by_region("Piemonte")
 population_num = get_number_population_by_region("Piemonte")
 population_dense = get_number_population_denstiy_by_region("Piemonte")
 region_area = get_number_area_by_region("Piemonte")
+smoker_share = get_number_smoker_share_by_region("Piemonte")
 
-s = f"population:{population_num} area:{region_area} density:{population_dense}"
+s = f"population:{population_num} area:{region_area} density:{population_dense} smokers share:{smoker_share}"
 print(s)
+
 
 region_population = get_list_population_by_region("Piemonte")
 (females, males) = get_age_range(region_population, 99, 100)
